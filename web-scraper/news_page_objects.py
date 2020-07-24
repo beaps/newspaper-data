afa567047a026b29939fcbf535bcdb5f6a0d51f6
Bea P. Santana <beapsant@gmail.com>
2020-07-24 20:00:25 +0100

    Get articles

--- web-scraper/news_page_objects.py
+++ web-scraper/news_page_objects.py
@@ -3,14 +3,14 @@ import bs4
 
 from common import config
 
-class HomePage:
+class NewsPage:
 	def __init__(self, news_site_uid, url):
 		self._config = config()['news_sites'][news_site_uid]
 		self._queries = self._config['queries']
 		self._html = None
 
 		self._visit(url)
-
+	
 	# Visitar la página
 	def _visit(self, url):
 		response = requests.get(url)
@@ -22,8 +22,12 @@ class HomePage:
 	# Obtener información del html que se ha parseado
 	def _select(self, query_string):
 		return self._html.select(query_string)
+
+class HomePage(NewsPage):
+	def __init__(self, news_site_uid, url):
+		super().__init__(news_site_uid, url)
 	
-	# Generar nuestra primera propiedad
+	# Generar una propiedad
 	@property
 	def article_links(self):
 		link_list = []
@@ -32,3 +36,18 @@ class HomePage:
 				link_list.append(link)
 		# Para no repetir links se crea un set
 		return set(link['href'] for link in link_list)
+
+class ArticlePage(NewsPage):
+	def __init__(self, news_site_uid, url):
+		super().__init__(news_site_uid, url)
+	
+	@property
+	def body(self):
+		result = self._select(self._queries['article_body'])
+		return result[0].text if len(result) else ''
+
+	@property
+	def title(self):
+		result = self._select(self._queries['article_title'])
+		return result[0].text if len(result) else ''
+
